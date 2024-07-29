92e247148e2ae60daf2daf49ac2b539d15ab74bb
Artur Tsoi <xzoyart@gmail.com>
2024-07-29 12:37:06 +0500

    Fix linter msg

--- app/src/main/java/hexlet/code/Main.java
+++ app/src/main/java/hexlet/code/Main.java
@@ -1,7 +1,5 @@
 package hexlet.code;
 
-import java.util.HashMap;
-
 public class Main {
     public static void main(String[] args) {
     }
--- app/src/main/java/hexlet/code/schemas/map/RequiredValidation.java
+++ app/src/main/java/hexlet/code/schemas/map/RequiredValidation.java
@@ -7,6 +7,6 @@ import java.util.Map;
 public class RequiredValidation<K, V> implements ValidationStrategy<Map<K, V>> {
     @Override
     public boolean validate(Map<K, V> map) {
-        return map != null ;
+        return map != null;
     }
 }
